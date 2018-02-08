e3cfb053d0afcf503efaf90ef555418c9c9a2859
Alexander Urizar <urizaraj@gmail.com>
2018-02-08 17:40:14 +0000

    Done.

---
+++ lib/current_player.rb
@@ -0,0 +1,11 @@
+def turn_count(board)
+  turn = 0
+  board.each do |value|
+    turn += 1 if ['X', 'O'].include?(value)
+  end
+  turn
+end
+
+def current_player(board)
+  turn_count(board).even? ? 'X' : 'O'
+end
